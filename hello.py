a88fdd82048fb1502b8effe7584dbe3f3a39a34e
SimantaKarki <50289166+SimantaKarki@users.noreply.github.com>
2020-03-08 21:26:24 +0545

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# list
+li = [1, 2, 3, 4, 5]
+li2 = ["Simanta" "Kushal" "Sirish" "Krishna"]
+li3 = [1, 2, "simanta"]
+print(li3[2])
+
+print(len(li))
+
+# list slicing
+item = ["laptops", "mobiles", "gears", "cosmetic", "random"]
+print(item[0:0:1])
