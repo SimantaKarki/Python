2f68b4c7ead637abdcd879fde84331975eac3c39
SimantaKarki <50289166+SimantaKarki@users.noreply.github.com>
2020-03-11 07:14:24 +0545

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,13 @@
+# lambda expression
+
+my_list = [1, 2, 3, ]
+
+# lambda param: action(param)
+
+
+# def multiply_by2():
+#     return item*2
+
+
+print(list(map(lambda item: item*2, my_list)))
+print(my_list)
